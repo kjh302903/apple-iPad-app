4b413851605746e04504314c8b05b5d0a6cea27d
kjh302903 <kjh302903@gmail.com>
2023-02-01 15:51:43 +0900

    media query(mobile mode) 추가

--- js/main.js
+++ js/main.js
@@ -43,12 +43,15 @@ const searchInputEl = searchWrapEl.querySelector('input');
 const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
 
 searchStarterEl.addEventListener('click', showSearch);
-searchCloserEl.addEventListener('click', hideSearch);
+searchCloserEl.addEventListener('click', (event) => {
+    event.stopPropagation();
+    hideSearch();
+});
 searchShadowEl.addEventListener('click', hideSearch);
 
 function showSearch() {
     headerEl.classList.add('searching');
-    document.documentElement.classList.add('fixed');
+    stopScroll();
     headerMenuEls.reverse().forEach((el, index) => {
         el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
     });
@@ -61,7 +64,7 @@ function showSearch() {
 }
 function hideSearch() {
     headerEl.classList.remove('searching');
-    document.documentElement.classList.remove('fixed');
+    playScroll();
     headerMenuEls.reverse().forEach((el, index) => {
         el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
     });
@@ -71,6 +74,51 @@ function hideSearch() {
     searchDelayEls.reverse();
     searchInputEl.value = "";
 }
+function playScroll() {
+    document.documentElement.classList.remove('fixed');
+}
+function stopScroll() {
+    document.documentElement.classList.add('fixed');
+}
+
+
+// 헤더 메뉴 토글
+const menuStarterEl = document.querySelector('header .menu-starter');
+menuStarterEl.addEventListener('click', () => {
+    if(headerEl.classList.contains('menuing')) {
+        headerEl.classList.remove('menuing');
+        searchInputEl.value = "";
+        playScroll();
+    }
+    else {
+        headerEl.classList.add('menuing');
+        stopScroll();
+    }
+    
+});
+
+// 헤더 검색
+const searchTextFieldEl = document.querySelector('header .textfield');
+const searchCancelEl = document.querySelector('header .search-canceler');
+searchTextFieldEl.addEventListener('click', () => {
+    headerEl.classList.add('searching-mobile');
+    searchInputEl.focus();
+});
+searchCancelEl.addEventListener('click', () => {
+    headerEl.classList.remove('searching-mobile');
+
+});
+
+//
+window.addEventListener('resieze',() => {
+    if(window.innerWidth <= 740) {
+        headerEl.classList.remove('searching');
+    }
+    else {
+        headerEl.classList.remove('searching-mobile');
+    }
+});
+
 
 //요소의 가시성 관찰
 const io = new IntersectionObserver((entries) => {
