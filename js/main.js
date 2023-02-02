4786e1e0ff5f629616539b22ecf025eb0477c594
kjh302903 <kjh302903@gmail.com>
2023-02-02 15:53:50 +0900

    media qeury(footer) 추가

--- js/main.js
+++ js/main.js
@@ -222,6 +222,7 @@ navigations.forEach((nav) => {
     mapEl.innerHTML = /*HTML*/`
         <h3>
             <span class = 'text'>${nav.title}</span>
+            <span class = 'icon'>+</span>
         </h3>
         <ul>
             ${mapList}
@@ -233,3 +234,11 @@ navigations.forEach((nav) => {
 
 const thisYearEl =  document.querySelector('span.this-year');
 thisYearEl.textContent = new Date().getFullYear();
+
+const mapEls = document.querySelectorAll('footer .navigations .map');
+mapEls.forEach((el) => {
+    const h3El = el.querySelector('h3');
+    h3El.addEventListener('click', () => {
+        el.classList.toggle('active');
+    });
+});
